e63b478f375f52587c10f2a7c539db3e979347e4
James <96896885+sunbok1025@users.noreply.github.com>
2025-07-30 02:14:30 +0900

    bash operator dag update

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -6,7 +6,7 @@ from airflow.sdk import DAG, chain
 with DAG(
     dag_id="dags_bash_operator", # airflow 관리도구에서 DAG 제목에 해당하는 부분(파일명도 동일하게 하는 게 좋음)
     schedule="0 0 * * *", # 크론 스케줄 시간 지정 
-    start_date=pendulum.datetime(2025, 7, 30, tz="Asian/Seoul"), # 스케줄 시작 날짜
+    start_date=pendulum.datetime(2025, 7, 30, tz="Asia/Seoul"), # 스케줄 시작 날짜
     catchup=False, # 시작날짜가 현재날짜보다 이전일 경우 소급 적용 여부
     tags=["example", "example2", "example3"], # DAG 필터링을 위한 태그 지정
 ) as dag:
