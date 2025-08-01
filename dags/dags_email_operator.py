3f79b7cfeb3152caba3f3a05b45bc0b16f319937
James <96896885+sunbok1025@users.noreply.github.com>
2025-08-02 01:55:02 +0900

    Create email operator

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,17 @@
+import pendulum
+
+from airflow.providers.smtp.operators.smtp import EmailOperator
+from airflow.sdk import DAG, chain
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2025, 7, 30, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='sunbok1025@naver.com',
+        subject='Airflow 성공 메일',
+        html_content='Airflow 작업이 완료되었습니다.'
+    )
