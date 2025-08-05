1369a5b028e5428d1d9af91e883e693ed02427ae
James <96896885+sunbok1025@users.noreply.github.com>
2025-08-05 23:45:07 +0900

    Create common python function

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,17 @@
+import pendulum
+
+from airflow.providers.standard.operators.python import PythonOperator
+from common.common_func import get_sftp
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id='dags_python_import_func',
+    schedule='30 6 * * *',
+    start_date=pendulum.datetime(2025, 7, 30, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
+    )
