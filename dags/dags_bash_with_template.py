36aa27e9883a2966c25d0ca43f5bbafb1e78e703
James <96896885+sunbok1025@users.noreply.github.com>
2025-08-07 20:39:59 +0900

    Modify bash operator with template

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -18,15 +18,15 @@ with DAG(
     bash_task2 = BashOperator(
         task_id='bash_task2',
         env={
-            'START_DATE':'{{data_interval_start | ds}}', # YYYY-MM-DD 형식으로 출력
-            'END_DATE':'{{data_interval_end | ds}}'
+            'START_DATE':'{{ data_interval_start | ds }}', # YYYY-MM-DD 형식으로 출력
+            'END_DATE':'{{ data_interval_end | ds }}'
         },
         bash_command='eho $START_DATE && $END_DATE'
     )
 
     bash_task3 = BashOperator(
         task_id='bash_task3',
-        bash_command='ehco run_id: {{run_id}}'
+        bash_command='ehco run_id: {{ run_id }}'
     )
 
     bash_task1 >> bash_task2 >> bash_task3    
