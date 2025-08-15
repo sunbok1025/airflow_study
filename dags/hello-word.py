5c813fcd5681af107ec6835b3f890a201ae567b8
James <96896885+sunbok1025@users.noreply.github.com>
2025-08-15 11:22:09 +0900

    Modify 1 hello-word.py

--- dags/hello-word.py
+++ dags/hello-word.py
@@ -1,6 +1,16 @@
 from airflow.sdk import asset
 
-
 @asset(schedule="@daily")
 def extracted_data():
     return {"a": 23, "b": 19}
+
+@asset(schedule=extracted_data)
+def transformed_data(context):
+    my_data = context["ti"].xcom_pull(
+        dag_id="extracted_data",
+        task_ids="extracted_data",
+        key="return_value",
+        include_prior_dates=True,
+    )
+    summed_data = my_data["a"] + my_data["b"]
+    return summed_data
